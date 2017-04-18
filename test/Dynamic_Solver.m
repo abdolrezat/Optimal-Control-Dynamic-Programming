6ebbe1a290fd56ffef38bac2b139378acffd5fd2
Reza Taheri <Art.nt.c4@gmail.com>
2017-04-18 13:42:12 +0430

    Fix test-linear branch conflict

--- test/Dynamic_Solver.m
+++ test/Dynamic_Solver.m
@@ -20,18 +20,34 @@ classdef Dynamic_Solver < handle
         dx
         du
         u_star
+        u_star_idx
         J_star
+        J_current_state
+        J_opt_nextstate
         X1_mesh
         X2_mesh
         X1_mesh_3D
         X2_mesh_3D
+        X_next_M1
+        X_next_M2
         U_mesh_3D
+        J_check
+        J_current_state_check 
+        J_opt_nextstate_check 
+        X_next_M1_check 
+        X_next_M2_check
+        checkstagesXJF
     end
     
     methods
         
         function obj = Dynamic_Solver()
+<<<<<<< HEAD:test/Dynamic_Solver.m
             obj.Q = [0.25, 0; 0, 0.05]*5;
+=======
+            obj.checkstagesXJF = 1;
+            obj.Q = [0.25, 0; 0, 0.05];
+>>>>>>> test-linear:Dynamic_Solver.m
             obj.A = [0.9974, 0.0539; -0.1078, 1.1591];
             obj.B = [0.0013; 0.0539];
             obj.R = 0.05*5;
@@ -59,12 +75,21 @@ classdef Dynamic_Solver < handle
             %3d grid for voctorization in calculation of C_star_M
             [obj.X1_mesh_3D,obj.X2_mesh_3D,obj.U_mesh_3D] = ndgrid(s_r,s_r,U_mesh);
             %
+<<<<<<< HEAD:test/Dynamic_Solver.m
             obj.J_star = zeros([size(obj.X1_mesh),obj.N],'single');
+=======
+%             obj.J_star = zeros([size(obj.X1_mesh),obj.N]);
+>>>>>>> test-linear:Dynamic_Solver.m
             obj.u_star = obj.J_star;
+            [obj.X_next_M1, obj.X_next_M2] = a_D_M(obj);
+            obj.J_current_state = g_D(obj);
+            obj.J_opt_nextstate = zeros(size(obj.X1_mesh));
+            
             % Increase K by 1
             for k=1:obj.N-1
                 tic
                 k_s = obj.N-k;
+<<<<<<< HEAD:test/Dynamic_Solver.m
                 J_M = J_state_M(obj, k);
 %                 switch k
 %                     case {3,20,60,100,129}
@@ -76,8 +101,11 @@ classdef Dynamic_Solver < handle
 %                         close gcf
 %                 end                
                 [obj.J_star(:,:,k_s),u_star_idx] = min(J_M,[],3);
+=======
+                J_state_M(obj, k);
+>>>>>>> test-linear:Dynamic_Solver.m
                 % store UMIN in UOPT(N-k,I)
-                obj.u_star(:,:,k_s) = U_mesh(u_star_idx);
+                obj.u_star(:,:,k_s) = U_mesh(obj.u_star_idx);
                 fprintf('step %d - %f seconds\n', k, toc)
             end %end of for loop when k = N
             
@@ -105,18 +133,18 @@ classdef Dynamic_Solver < handle
                 
                 U(k) = Fu(X(1,k),X(2,k));
                 
-                Fj = griddedInterpolant(obj.X1_mesh, obj.X2_mesh,...
-                    obj.J_star(:,:,k),'linear');
-                J(k) = Fj(X(1,k),X(2,k));
+%                 Fj = griddedInterpolant(obj.X1_mesh, obj.X2_mesh,...
+%                     obj.J_star(:,:,k),'linear');
+%                 J(k) = Fj(X(1,k),X(2,k));
                 
                 X(:,k+1) = a_D(obj,X(1,k),X(2,k),U(k));
                 % X(:,k+1) = obj.A*X(:,k) + obj.B*U(k);
             end
             k = k+1;
             %-- Optimal Control Input u*
-            Fu = griddedInterpolant(obj.X1_mesh, obj.X2_mesh,...
-                obj.u_star(:,:,k),'linear');
-            U(k) = Fu(X(1,k),X(2,k));
+%             Fu = griddedInterpolant(obj.X1_mesh, obj.X2_mesh,...
+%                 obj.u_star(:,:,k),'linear');
+%             U(k) = Fu(X(1,k),X(2,k));
             
             %-- Commented -- Cost of path
             %Fj = griddedInterpolant(obj.X1_mesh, obj.X2_mesh,...
@@ -145,7 +173,7 @@ classdef Dynamic_Solver < handle
         end
         
         
-        function X1_new = a_D(obj,X1,X2,Ui) 
+        function X1_new = a_D(obj,X1,X2,Ui)
             % old function used for get_optimal_path
             X1_new = obj.A*[X1;X2] + obj.B*Ui;
         end
@@ -153,18 +181,48 @@ classdef Dynamic_Solver < handle
         function J = g_D(obj)
             %J = [X1;X2]' * obj.Q * [X1;X2] + Ui' * obj.R * Ui;
             J = obj.Q(1)*obj.X1_mesh_3D.^2 + ...
-                    obj.Q(4)*obj.X2_mesh_3D.^2 + obj.R * obj.U_mesh_3D.^2;
+                obj.Q(4)*obj.X2_mesh_3D.^2 + obj.R * obj.U_mesh_3D.^2;
         end
         
-        function J = J_state_M(obj,k)
+        function J_state_M(obj,k)
             F = griddedInterpolant(obj.X1_mesh, obj.X2_mesh,...
-                obj.J_star(:,:,obj.N-k+1),'linear');
+                obj.J_opt_nextstate,'linear');
             %get next state X
-            [X_next_M1,X_next_M2] = a_D_M(obj);
+%             [X_next_M1,X_next_M2] = a_D_M(obj);
             %find J final for each state and control (X,U) and add it to next state
             %optimum J*
-            J = F(X_next_M1,X_next_M2) + g_D(obj);
+            
+            % % Add up J's % % 
+            [obj.J_opt_nextstate, obj.u_star_idx] = ...
+                min(F(obj.X_next_M1, obj.X_next_M2) + obj.J_current_state,[],3);
+            % % % % % % % % % %
+            if(obj.checkstagesXJF)
+                obj.J_current_state_check(:,:,k) = obj.J_current_state(50:55,52:57,105);
+%                 obj.J_opt_nextstate_check(:,:,k) = obj.J_opt_nextstate(50:55,52:57,105);
+                obj.X_next_M1_check(:,:,k) = obj.X_next_M1(50:55,52:57,105);
+                obj.X_next_M2_check(:,:,k)  = obj.X_next_M2(50:55,52:57,105);
+%                 obj.J_check(:,:,k) = obj.J_opt_nextstate(50:55,52:57,105);
+            end
         end
+        
+        function compare_stages(obj, k)
+            fprintf('%% -------------------------------------------\n')
+            fprintf('J for stages\n')
+            obj.J_check(:,:,k)
+            fprintf('%% -----------------------------------------------\n')
+            fprintf('J Current stage for stages\n')
+            obj.J_current_state_check(:,:,k)
+            fprintf('%% -------------------------------------------\n')
+            fprintf('J optimum next stage for stages\n')
+            obj.J_opt_nextstate_check(:,:,k)
+            fprintf('%% -------------------------------------------\n')            
+            fprintf('X1 next stage\n')
+            obj.X_next_M1_check(:,:,k)
+            fprintf('%% -------------------------------------------\n')
+            fprintf('X2 next stage\n')
+            obj.X_next_M2_check(:,:,k)
+        end
+        
     end
     
     methods (Static)
